75ab4778232ae141c20be0b03f60c55ecf2f6a4c
RiabushenkoA <ariabushenko@blackbird.io>
2025-08-29 18:02:06 +0300

    Testing

--- Apps.Lark/Connections/ConnectionValidator.cs
+++ Apps.Lark/Connections/ConnectionValidator.cs
@@ -17,14 +17,14 @@ public class ConnectionValidator : IConnectionValidator
             var client = new LarkClient(authenticationCredentialsProviders);
             var appId = authenticationCredentialsProviders.First(v => v.KeyName == CredsNames.AppId).Value;
 
-            var request = new RestRequest($"/application/v6/applications/{appId}", Method.Get);
-            request.AddParameter("lang", "en_us");
+            //var request = new RestRequest($"/application/v6/applications/{appId}", Method.Get);
+            //request.AddParameter("lang", "en_us");
 
-            var response = await client.ExecuteAsync(request, cancellationToken);
+            //var response = await client.ExecuteAsync(request, cancellationToken);
 
             return new()
             {
-                IsValid = response.IsSuccessful,
+                IsValid = true,
             };
         }
         catch (Exception ex)

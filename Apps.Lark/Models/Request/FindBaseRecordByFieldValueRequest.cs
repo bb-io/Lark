ec60b7f9089be79ec3c99e5d3eb04933981f8fdb
Artem Riabushenko <ariabushenko@blackbird.io>
2026-01-06 19:16:56 +0200

    Added new action 'Find base record by field value'

---
+++ Apps.Lark/Models/Request/FindBaseRecordByFieldValueRequest.cs
@@ -0,0 +1,16 @@
+﻿using Apps.Lark.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.Lark.Models.Request
+{
+    public class FindBaseRecordByFieldValueRequest
+    {
+        [Display("Field ID")]
+        [DataSource(typeof(BaseTableFieldDataSourceHandler))]
+        public string FieldId { get; set; }
+
+        [Display("Value")]
+        public string Value { get; set; } = string.Empty;
+    }
+}
